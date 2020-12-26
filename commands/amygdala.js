cddcd9d5e7af076b0da718ed036858e6d6b0d1b6
Yasmeen <yasmeenhmaidan@hotmail.com>
2020-12-26 16:03:16 -0500

    implemented amygdala command

---
+++ commands/amygdala.js
@@ -0,0 +1,25 @@
+const { execute } = require("./on");
+
+module.exports = {
+    name: 'amygdala',
+    description: 'info on the amygdala',
+    execute(message, args, Discord){
+        const newAmygdala = new Discord.MessageEmbed()
+        .setColor('#EE2677')
+        .setTitle('Amygdala')
+        .setURL()
+        .setDescription('What is the amygdala?')
+        .addFields(
+
+            {name: 'Brain Region:', value:'Limbic System'},
+            {name: 'Function:', value:'Emotions'},
+            {name: 'Motto', value:'Fight or Flight'}
+
+        )
+        .setImage('https://upload.wikimedia.org/wikipedia/commons/2/2f/MRI_Location_Amygdala_up.png')
+        .setFooter('The Brain Encyclopedia retrieved the Amygdala');
+        
+        message.channel.send(newAmygdala);
+    }
+    
+}
