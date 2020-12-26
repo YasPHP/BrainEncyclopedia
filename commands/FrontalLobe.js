042ca5764c28fb9193c94098d7041f71c1393daf
Yasmeen <yasmeenhmaidan@hotmail.com>
2020-12-26 16:58:57 -0500

    implemented frontal lobe command

---
+++ commands/FrontalLobe.js
@@ -0,0 +1,24 @@
+const { execute } = require("./on");
+
+module.exports = {
+    name: 'FrontalLobe',
+    description: 'info on the amygdala',
+    execute(message, args, Discord){
+        const newAFrontalLobe = new Discord.MessageEmbed()
+        .setColor('#FC773E')
+        .setTitle('Frontal Lobe')
+        .setURL()
+        .setDescription('What is the Frontal Lobe?')
+        .addFields(
+
+            {name: 'Brain Region:', value:'Limbic System'},
+            {name: 'Function:', value:'Emotions'},
+            {name: 'Motto', value:'Fight or Flight'}
+
+        )
+        .setImage('https://upload.wikimedia.org/wikipedia/commons/2/2f/MRI_Location_Amygdala_up.png')
+        .setFooter('The Brain Encyclopedia retrieved the Frontal Lobe');
+        
+        message.channel.send(newFrontalLobe);
+    }  
+}
