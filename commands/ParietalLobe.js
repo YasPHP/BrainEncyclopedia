21e7cf3a12b03a5ea715cf53f6d0c4b6d9b08e60
Yasmeen <yasmeenhmaidan@hotmail.com>
2020-12-26 17:50:25 -0500

    parietal info

--- commands/ParietalLobe.js
+++ commands/ParietalLobe.js
@@ -11,13 +11,13 @@ module.exports = {
         .setDescription('What is the Parietal Lobe?')
         .addFields(
 
-            {name: 'Brain Region:', value:'Limbic System'},
-            {name: 'Function:', value:'Emotions'},
-            {name: 'Motto', value:'Fight or Flight'}
+            {name: 'Brain Region:', value:'Above the temporal lobe and behind the frontal lobe and central sulcus.'},
+            {name: 'Function:', value:'Integrating sensory information from various parts of the body, knowledge of numbers and their relations, and in the manipulation of objects.'},
+            {name: 'Motto:', value:'Sensory Perception'}
 
         )
-        .setImage('https://upload.wikimedia.org/wikipedia/commons/2/2f/MRI_Location_Amygdala_up.png')
-        .setFooter('The Brain Encyclopedia retrieved the Parietal Lobe');
+        .setImage('https://upload.wikimedia.org/wikipedia/commons/3/32/Parietal_lobe_animation_small.gif')
+        .setFooter('The Brain Encyclopedia retrieved the Parietal Lobe.');
         
         message.channel.send(newParietalLobe);
     }  
