3b20c5c3623f9a4712b4803829d8fdc27f365dc3
Yasmeen <yasmeenhmaidan@hotmail.com>
2020-12-26 15:23:31 -0500

    capitalized global constants
    Javascript Convention:
    
    Global constants whose value doesn't change should not be named like normal variables.

--- main.js
+++ main.js
@@ -1,45 +1,45 @@
-const Discord = require('discord.js');
+const DISCORD = require('discord.js');
 
-const client = new Discord.Client();
+const CLIENT = new DISCORD.CLIENT();
 
 // what user needs to type to use discord bot commands
-const prefix = '-';
+const PREFIX = '-';
 
 const fs = require('fs');
 
 // advanced command handler collection
-client.commands = new Discord.Collection();
+CLIENT.commands = new DISCORD.Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles){
     const command = require(`./commands/${file}`);
 
-    client.commands.set(command.name, command);
+    CLIENT.commands.set(command.name, command);
 }
 
 
 
 // bot is online!
 
-client.once('ready', () => {
+CLIENT.once('ready', () => {
     console.log('BrainEncyclopedia is here!');
 });
 
 
-client.on('message', message =>{
+CLIENT.on('message', message =>{
 
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(PREFIX) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
+    const args = message.content.slice(PREFIX.length).split(/ +/);
 
     const command = args.shift().toLowerCase();
 
     // use the advanced command handler!
     if (command === 'on'){
-        client.commands.get('on').execute(message, args);
+        CLIENT.commands.get('on').execute(message, args);
     } else if (command === 'off'){
-        client.commands.get('off').execute(message, args);
+        CLIENT.commands.get('off').execute(message, args);
     }
 
 });
@@ -47,4 +47,4 @@ client.on('message', message =>{
 
 
 // the bot is logged in with its token (SECRET COMBO #COME BACK & ADD TO GITIGNORE MAYBE: https://www.reddit.com/r/learnpython/comments/7pzlm0/discord_bot_token_on_github/ !)
-client.login('TOKEN');
+CLIENT.login('TOKEN');
