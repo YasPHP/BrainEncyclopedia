eb001fed139f3da78b69f0c192dd93170eebb26d
Yasmeen <yasmeenhmaidan@hotmail.com>
2020-12-26 17:00:25 -0500

    underscored name
    - command req is lower case

--- commands/KhanAcademy.js
+++ commands/KhanAcademy.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'khanacademy',
+    name: 'khan_academy',
     description: "sends khan academy's neurology playlist",
     execute(message, args){
         message.channel.send('https://www.youtube.com/playlist?list=PL1BYme_lOx_plW8FR9jOxy-FVQ-h_omIX');
