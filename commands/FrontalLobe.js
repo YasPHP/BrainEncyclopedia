c5e011a042768245787ffb143acfef0f2685bfbe
Yasmeen <yasmeenhmaidan@hotmail.com>
2020-12-26 17:50:42 -0500

    frontal lobe info

--- commands/FrontalLobe.js
+++ commands/FrontalLobe.js
@@ -11,13 +11,13 @@ module.exports = {
         .setDescription('What is the Frontal Lobe?')
         .addFields(
 
-            {name: 'Brain Region:', value:'Limbic System'},
-            {name: 'Function:', value:'Emotions'},
-            {name: 'Motto', value:'Fight or Flight'}
+            {name: 'Brain Region:', value:'Includes the premotor cortex, and the primary motor cortex – cortical parts of the motor cortex.'},
+            {name: 'Function:', value:'Skeletal movement, ocular movement, speech control, the expression of emotions, etc.'},
+            {name: 'Motto:', value:'The Action Cortex'}
 
         )
-        .setImage('https://upload.wikimedia.org/wikipedia/commons/2/2f/MRI_Location_Amygdala_up.png')
-        .setFooter('The Brain Encyclopedia retrieved the Frontal Lobe');
+        .setImage('https://upload.wikimedia.org/wikipedia/commons/5/54/Frontal_lobe_animation.gif')
+        .setFooter('The Brain Encyclopedia retrieved the Frontal Lobe.');
         
         message.channel.send(newFrontalLobe);
     }  
