e3de1a894e7b536fcd5f48ce83ee9e038f57425e
Yasmeen <yasmeenhmaidan@hotmail.com>
2021-01-02 00:28:27 -0500

    added FoldIt command

---
+++ commands/FoldIt.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'fold_it',
+    description: "research: protein research reconstruction",
+    execute(message, args){
+        message.channel.send('https://fold.it/portal/info/about');
+    }
+}
