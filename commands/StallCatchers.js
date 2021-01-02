d79248cbd577848bf209e7a06a419ebd306b6878
Yasmeen <yasmeenhmaidan@hotmail.com>
2021-01-01 23:44:24 -0500

    stall catchers resource command

---
+++ commands/StallCatchers.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'stall_catchers',
+    description: "sends a collecton of neuroscience crowdscience research platforms",
+    execute(message, args){
+        message.channel.send('https://stallcatchers.com/main ');
+    }
+}
