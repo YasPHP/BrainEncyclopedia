e7498e3f7d342fc21b94d50ee458217f5f989fbd
Yasmeen <yasmeenhmaidan@hotmail.com>
2021-01-02 00:28:38 -0500

    implemented eyewire command

---
+++ commands/eyewire.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'eyewire',
+    description: "research: map the 3D structure of neurons",
+    execute(message, args){
+        message.channel.send('https://eyewire.org/explore');
+    }
+}
