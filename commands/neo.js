f945ac6e784b95132cf6b347667e25de172d55c0
Yasmeen <yasmeenhmaidan@hotmail.com>
2021-01-02 00:27:55 -0500

    neo command added

---
+++ commands/neo.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'neo',
+    description: "research: gamers will uncover deep secrets of the brain by solving 3D puzzles",
+    execute(message, args){
+        message.channel.send('https://neo.eyewire.org/');
+    }
+}
