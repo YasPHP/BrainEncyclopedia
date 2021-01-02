ec229f697f34119be80819b983fe34de923ccbba
Yasmeen <yasmeenhmaidan@hotmail.com>
2021-01-02 00:28:04 -0500

    implemented mozak comand

---
+++ commands/mozak.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'mozak',
+    description: "research: discovering new 3-dimensional neuronal arborization patterns, eventually matching them to firing and genetic patterns, and then simulating them in the game",
+    execute(message, args){
+        message.channel.send('https://www.mozak.science/landing');
+    }
+}
